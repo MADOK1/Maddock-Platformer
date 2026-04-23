512bb9ef3feb3e52ad50f29d82da753b2bb82d43
Maddock Pearson <99259670+MADOK1@users.noreply.github.com>
2026-04-22 20:39:07 -0400

    add main menu

--- Levels/main_menu.gd
+++ Levels/main_menu.gd
@@ -4,3 +4,11 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$MaddockBones/AnimationTree/AnimationPlayer.play("mainmenugifdance")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Levels/trenchbroom_loader_test.tscn")
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Levels/wuhu_island.tscn")
